3ba321a8f1a1cf19f750d2853d04a27011822a71
arikakira <samanthal242@nycstudents.net>
2024-10-24 11:27:45 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,14 @@
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args)
+    {
+        WordChecker w = new WordChecker();
+        ArrayList<String> words = new ArrayList<>();
+        words.add("an");
+        words.add("band");
+        words.add("band");
+        words.add("abandon");
+        System.out.print(words);
+    }
+}
