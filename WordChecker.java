3dd27a85361a2261bd55927ee4d4f0e51d1a6eaa
arikakira <samanthal242@nycstudents.net>
2024-10-24 11:28:14 -0400

    Update WordChecker.java

--- WordChecker.java
+++ WordChecker.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+
 public class WordChecker {
     /** Initialized in the constructor and contains no null elements */
     private ArrayList<String> wordList;
+
+    public WordChecker(ArrayList<String> list)
+    {
+        wordList = list;
+    }
+
+    public WordChecker() {
+        wordList = new ArrayList<String>();
+    }
 
     /**
      * Returns true if each element of wordList (except the first) contains the
@@ -9,8 +20,8 @@ public class WordChecker {
      * Precondition: wordList contains at least two elements.
      * Postcondition: wordList is unchanged.
      */
-    public boolean isWordChain() {
-        /* to be implemented in part (a) */ }
+    // public boolean isWordChain() {
+    //     /* to be implemented in part (a) */ }
 
     /**
      * Returns an ArrayList<String> based on strings from wordList that start
@@ -21,6 +32,6 @@ public class WordChecker {
      * Items appear in the returned list in the same order as they appear in
      * wordList.
      */
-    public ArrayList<String> createList(String target) {
-        /* to be implemented in part (b) */ }
+    // public ArrayList<String> createList(String target) {
+    //     /* to be implemented in part (b) */ }
 }
