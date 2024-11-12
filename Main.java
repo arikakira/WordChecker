28f37d56db07b72575a5c62b62b5d095fb96cc8c
arikakira <samanthal242@nycstudents.net>
2024-11-12 11:50:08 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args)
     {
-        WordChecker w = new WordChecker();
         ArrayList<String> words = new ArrayList<String>();
         words.add("an");
         words.add("band");
@@ -11,5 +10,17 @@ public class Main {
         words.add("abandon");
         System.out.println(words);
         WordChecker x = new WordChecker(words);
+        System.out.println(x.isWordChain());
+
+        ArrayList<String> moreWords = new ArrayList<String>();
+        moreWords.add("to");
+        moreWords.add("too");
+        moreWords.add("stool");
+        moreWords.add("tools");
+        System.out.println(moreWords);
+        WordChecker y = new WordChecker(moreWords);
+        System.out.println(y.isWordChain());
+
+        
     }
 }
