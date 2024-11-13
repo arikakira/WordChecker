2f6476714b3c0a4b465421f6a74dd9f988b33127
arikakira <samanthal242@nycstudents.net>
2024-11-12 23:39:00 -0500

    Update WordChecker.java

--- WordChecker.java
+++ WordChecker.java
@@ -41,6 +41,12 @@ public class WordChecker {
      * wordList.
      */
     public ArrayList<String> createList(String target) {
-        
+        ArrayList<String> list = new ArrayList<String>();
+        for (String s: wordList) {
+            if(s.indexOf(target) == 0) {
+                list.add(s.substring(target.length()));
+            }
+        }
+        return list;
     }
 }
