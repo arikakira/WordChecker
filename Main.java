374b50b964a14a7d94c53daee8d7a3ecd587c443
arikakira <samanthal242@nycstudents.net>
2024-11-12 23:38:33 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
         words.add("abandon");
         System.out.println(words);
         WordChecker x = new WordChecker(words);
-        System.out.println(x.isWordChain());
+        System.out.println(x.isWordChain());        // test case
 
         ArrayList<String> moreWords = new ArrayList<String>();
         moreWords.add("to");
@@ -19,8 +19,18 @@ public class Main {
         moreWords.add("tools");
         System.out.println(moreWords);
         WordChecker y = new WordChecker(moreWords);
-        System.out.println(y.isWordChain());
+        System.out.println(y.isWordChain());        // test case
 
-        
+        ArrayList<String> catchList = new ArrayList<String>();
+        catchList.add("catch");
+        catchList.add("bobcat");
+        catchList.add("catchacat");
+        catchList.add("cat");
+        catchList.add("at");
+        System.out.println(catchList);
+        WordChecker u = new WordChecker(catchList);
+        System.out.println(u.createList("cat"));        // test case
+        System.out.println(u.createList("catch"));      // test case
+        System.out.println(u.createList("dog"));        // test case
     }
 }
